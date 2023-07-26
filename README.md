8b3f4c34ff5823f2dfec4a3f3df0b2666252fbb2
Mehmet Büyükgümüş <62846517+mehmetbuyukgumus@users.noreply.github.com>
2023-07-26 19:29:01 +0200

    Editing Readme file

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+- [Introduction](#introduction)
+- [How to run this project](#how-to-run-this-project)
+- [Galery](#galery)
 
-## Getting Started
 
-First, run the development server:
+# Introduction
+
+It is a currency converter project. You can add or subtract the currency unit you want. Exchange rate data in the project was taken from exchangeratesapi.io
+
+# How to run this project
 
 ```bash
 npm run dev
@@ -14,21 +19,8 @@ pnpm dev
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
+You must add API Key to page.js in 16.line.
 
-This project uses [`next/font`](https://nextjs.org/docs/basic-features/font-optimization) to automatically optimize and load Inter, a custom Google Font.
+# Galery
 
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+![](./docs/ss.png)
--- app/page.js
+++ app/page.js
@@ -6,21 +6,23 @@ import axios from 'axios';
 
 export default function Home() {
     const [amount, setamount] = React.useState(0);
+    const [convertedAmount, setConvertedAmount] = React.useState(0);
     const [fromCurrency, setfromCurrency] = React.useState("USD");
     const [toCurrency, settoCurrency] = React.useState("TRY");
+    const currencies = ["TRY", "EUR"]
+    
 
     function getRate(fromCurrency, toCurrency) {
-        const url = "http://api.exchangeratesapi.io/v1/latest?access_key=API_KEY"
+        const url = "http://api.exchangeratesapi.io/v1/latest?access_key=4fdef46479e7a6d325425905428c9270"
         const endpoint = `${url}&base=${fromCurrency}&symbols=${toCurrency},`;
         axios.get(endpoint)
             .then(function (res) {
                 console.log(res);
                 const convertedValue = res.data.rates[toCurrency];
-                setamount(convertedValue * amount);
+                setConvertedAmount(convertedValue * amount);
             })
             .catch(function (err) {
                 console.log(err);
-                setamount(0);
             });
     }
 
@@ -30,17 +32,14 @@ export default function Home() {
 
     function updateAmount(event) {
         setamount(event.target.value);
-        convertCurrency();
     }
 
     function updateFromCurrency(event) {
         setfromCurrency(event.target.value);
-        convertCurrency();
     }
 
     function updateToCurrency(event) {
         settoCurrency(event.target.value);
-        convertCurrency();
     }
 
     return (
@@ -48,21 +47,22 @@ export default function Home() {
             <h3>Currency Converter</h3>
             <div className="container">
                 <div className="input-container">
-                    <input type="text" id="amountInput" placeholder="Miktar Giriniz..." onChange={updateAmount}></input>
+                    <input type="text" id="amountInput" value={amount} onChange={updateAmount}></input>
                     <select id="fromCurrency" onChange={updateFromCurrency}>
-                        <option value="USD">USD</option>
-                        <option value="TRY">TRY</option>
-                        <option value="EUR">EUR</option>
+                    {currencies.map((curr, index) => (
+                    <option value={curr}>{curr}</option>    
+                    ))}
                     </select>
                 </div>
                 <div className="input-container">
-                    <input type="text" id="resultInput" placeholder="Sonuç" value={amount} disabled></input>
+                    <input type="text" id="resultInput" placeholder="Sonuç" value={convertedAmount} disabled></input>
                     <select id="toCurrency" onChange={updateToCurrency}>
-                        <option value="TRY">TRY</option>
-                        <option value="EUR">EUR</option>
-                        <option value="USD">USD</option>
+                    {currencies.map((curr, index) => (
+                    <option value={curr}>{curr}</option> 
+            ))}
                     </select>
                 </div>
+            <button onClick={convertCurrency}>Çevir</button>
              
             </div>
         </div>
