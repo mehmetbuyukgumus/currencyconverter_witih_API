9b772f0420ccfac7831e5e7f0c38a7af76e44496
Mehmet Büyükgümüş <62846517+mehmetbuyukgumus@users.noreply.github.com>
2023-07-26 18:16:00 +0200

    Adding API

--- app/page.js
+++ app/page.js
@@ -1,33 +1,45 @@
 'use client'
-
 import style from '../styles/style.css'
 import React from "react";
+import axios from 'axios';
 
 export default function Home() {
     const [amount, setamount] = React.useState(0);
     const [fromCurrency, setfromCurrency] = React.useState("USD");
     const [toCurrency, settoCurrency] = React.useState("TRY");
 
+    function getRate(fromCurrency, toCurrency) {
+        const url = "http://api.exchangeratesapi.io/v1/latest?access_key=4fdef46479e7a6d325425905428c9270"
+        const endpoint = `${url}&base=${fromCurrency}&symbols=${toCurrency},`;
+        axios.get(endpoint)
+            .then(function (res) {
+                console.log(res);
+                const convertedValue = res.data.rates[toCurrency];
+                setamount(convertedValue * amount);
+            })
+            .catch(function (err) {
+                console.log(err);
+                setamount(0);
+            });
+    }
+
     function convertCurrency() {
-        const rates = {
-            TRY: 1,
-            USD: 28,
-            EUR: 30
-        };
-        const result = amount * rates[fromCurrency] / rates[toCurrency];
-        return result.toFixed(2);
+        getRate(fromCurrency, toCurrency);
     }
 
-    function updateAmout(event) {
+    function updateAmount(event) {
         setamount(event.target.value);
+        convertCurrency();
     }
 
-    function updatefromCurrency(event) {
+    function updateFromCurrency(event) {
         setfromCurrency(event.target.value);
+        convertCurrency();
     }
 
-    function updatetoCurrency(event) {
+    function updateToCurrency(event) {
         settoCurrency(event.target.value);
+        convertCurrency();
     }
 
     return (
@@ -35,19 +47,19 @@ export default function Home() {
             <h3>Currency Converter</h3>
             <div className="container">
                 <div className="input-container">
-                    <input type="text" id="amountInput" placeholder="Miktar Giriniz..." onChange={updateAmout}></input>
-                    <select id="fromCurrency" onChange={updatefromCurrency}>
+                    <input type="text" id="amountInput" placeholder="Miktar Giriniz..." onChange={updateAmount}></input>
+                    <select id="fromCurrency" onChange={updateFromCurrency}>
                         <option value="USD">USD</option>
-                        <option value="EUR">EUR</option>
                         <option value="TRY">TRY</option>
+                        <option value="EUR">EUR</option>
                     </select>
                 </div>
                 <div className="input-container">
-                    <input type="text" id="resultInput" placeholder="Sonuç" value={convertCurrency()} disabled></input>
-                    <select id="toCurrency" onChange={updatetoCurrency}>
-                        <option value="USD">USD</option>
-                        <option value="EUR">EUR</option>
+                    <input type="text" id="resultInput" placeholder="Sonuç" value={amount} disabled></input>
+                    <select id="toCurrency" onChange={updateToCurrency}>
                         <option value="TRY">TRY</option>
+                        <option value="EUR">EUR</option>
+                        <option value="USD">USD</option>
                     </select>
                 </div>
              
